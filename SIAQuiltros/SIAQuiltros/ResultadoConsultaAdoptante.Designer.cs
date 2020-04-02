608d48b44f5aa28ff261788276acb3f97d0b5ce4
Germán Contreras <german.contrerasa@utem.cl>
2020-04-02 19:02:57 -0300

    Se añadio las querys a ficha mascota

--- SIAQuiltros/SIAQuiltros/ResultadoConsultaAdoptante.Designer.cs
+++ SIAQuiltros/SIAQuiltros/ResultadoConsultaAdoptante.Designer.cs
@@ -31,18 +31,18 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResultadoConsultaAdoptante));
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.Comuna = new System.Windows.Forms.TextBox();
+            this.Rut = new System.Windows.Forms.TextBox();
+            this.Phone = new System.Windows.Forms.TextBox();
+            this.Name = new System.Windows.Forms.TextBox();
             this.Address = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.VoluntarioACargo = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.Cerrar = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
@@ -60,10 +60,10 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox5);
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.textBox3);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.Comuna);
+            this.groupBox1.Controls.Add(this.Rut);
+            this.groupBox1.Controls.Add(this.Phone);
+            this.groupBox1.Controls.Add(this.Name);
             this.groupBox1.Controls.Add(this.Address);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label5);
@@ -77,41 +77,52 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Antecedentes personales";
             // 
-            // label2
+            // Comuna
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 30);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(27, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Rut:";
+            this.Comuna.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Comuna.Enabled = false;
+            this.Comuna.Location = new System.Drawing.Point(67, 238);
+            this.Comuna.Name = "Comuna";
+            this.Comuna.Size = new System.Drawing.Size(100, 20);
+            this.Comuna.TabIndex = 9;
             // 
-            // label3
+            // Rut
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 60);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Nombre:";
+            this.Rut.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Rut.Enabled = false;
+            this.Rut.Location = new System.Drawing.Point(67, 27);
+            this.Rut.Name = "Rut";
+            this.Rut.Size = new System.Drawing.Size(100, 20);
+            this.Rut.TabIndex = 8;
             // 
-            // label4
+            // Phone
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 90);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 13);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Dirección:";
+            this.Phone.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Phone.Enabled = false;
+            this.Phone.Location = new System.Drawing.Point(67, 208);
+            this.Phone.Name = "Phone";
+            this.Phone.Size = new System.Drawing.Size(100, 20);
+            this.Phone.TabIndex = 7;
             // 
-            // label5
+            // Name
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 211);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 13);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "Teléfono:";
+            this.Name.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Name.Enabled = false;
+            this.Name.Location = new System.Drawing.Point(67, 57);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(242, 20);
+            this.Name.TabIndex = 6;
+            // 
+            // Address
+            // 
+            this.Address.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Address.Enabled = false;
+            this.Address.Location = new System.Drawing.Point(9, 106);
+            this.Address.Multiline = true;
+            this.Address.Name = "Address";
+            this.Address.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Address.Size = new System.Drawing.Size(300, 84);
+            this.Address.TabIndex = 5;
             // 
             // label6
             // 
@@ -122,42 +133,41 @@
             this.label6.TabIndex = 4;
             this.label6.Text = "Comuna:";
             // 
-            // Address
+            // label5
             // 
-            this.Address.Location = new System.Drawing.Point(9, 106);
-            this.Address.Multiline = true;
-            this.Address.Name = "Address";
-            this.Address.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.Address.Size = new System.Drawing.Size(300, 84);
-            this.Address.TabIndex = 5;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Teléfono:";
             // 
-            // textBox2
+            // label4
             // 
-            this.textBox2.Location = new System.Drawing.Point(67, 57);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(242, 20);
-            this.textBox2.TabIndex = 6;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Dirección:";
             // 
-            // textBox3
+            // label3
             // 
-            this.textBox3.Location = new System.Drawing.Point(67, 208);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 7;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Nombre:";
             // 
-            // textBox4
+            // label2
             // 
-            this.textBox4.Location = new System.Drawing.Point(67, 27);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 8;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(67, 238);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 9;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Rut:";
             // 
             // label7
             // 
@@ -168,12 +178,14 @@
             this.label7.TabIndex = 2;
             this.label7.Text = "Voluntario a cargo:";
             // 
-            // textBox1
+            // VoluntarioACargo
             // 
-            this.textBox1.Location = new System.Drawing.Point(120, 56);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(216, 20);
-            this.textBox1.TabIndex = 3;
+            this.VoluntarioACargo.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.VoluntarioACargo.Enabled = false;
+            this.VoluntarioACargo.Location = new System.Drawing.Point(120, 56);
+            this.VoluntarioACargo.Name = "VoluntarioACargo";
+            this.VoluntarioACargo.Size = new System.Drawing.Size(216, 20);
+            this.VoluntarioACargo.TabIndex = 3;
             // 
             // button1
             // 
@@ -201,12 +213,11 @@
             this.ClientSize = new System.Drawing.Size(349, 432);
             this.Controls.Add(this.Cerrar);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.VoluntarioACargo);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "ResultadoConsultaAdoptante";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ficha de Adoptante";
             this.Load += new System.EventHandler(this.ResultadoConsultaAdoptante_Load);
@@ -222,17 +233,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox Comuna;
+        private System.Windows.Forms.TextBox Rut;
+        private System.Windows.Forms.TextBox Phone;
+        private System.Windows.Forms.TextBox Name;
         private System.Windows.Forms.TextBox Address;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox VoluntarioACargo;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button Cerrar;
     }
--- SIAQuiltros/SIAQuiltros/ResultadoConsultaAdoptante.cs
+++ SIAQuiltros/SIAQuiltros/ResultadoConsultaAdoptante.cs
@@ -7,18 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace SIAQuiltros
 {
     public partial class ResultadoConsultaAdoptante : Form
     {
-        public void ShowInfo()
+        public void ShowInfo(String data)
         {
+            try
+            {
+                SqlConnection con = new SqlConnection("server=AMADEUS ; database=QUILTROS ; integrated security=True");
+                con.Open();
+                String qry = "SELECT *FROM ADOPTANTE WHERE nombre='" + data + "'";
+                SqlCommand cmd = new SqlCommand(qry, con);
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if(rdr.Read())
+                {
+                    Rut.Text = rdr["rut"].ToString();
+                    Name.Text = rdr["nombre"].ToString();
+                    Address.Text = rdr["direccion"].ToString();
+                    Phone.Text = rdr["telefono"].ToString();
+                    Comuna.Text = rdr["comuna"].ToString();
+                    con.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
 
         }
         public ResultadoConsultaAdoptante(String data)
         {
+            
             InitializeComponent();
+            ShowInfo(data);
         }
 
         private void ResultadoConsultaAdoptante_Load(object sender, EventArgs e)
