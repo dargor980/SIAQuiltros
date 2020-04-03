3282a889d6ea62ebc6b90506f922da3ba37f1d97
Germán Contreras <german.contrerasa@utem.cl>
2020-04-03 18:13:31 -0300

    Se modificó el reporte de donaciones.

--- SIAQuiltros/SIAQuiltros/Mostrar Lista de Donaciones.Designer.cs
+++ SIAQuiltros/SIAQuiltros/Mostrar Lista de Donaciones.Designer.cs
@@ -71,7 +71,7 @@
             this.dataGridView1.Location = new System.Drawing.Point(12, 53);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(443, 431);
+            this.dataGridView1.Size = new System.Drawing.Size(443, 455);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
